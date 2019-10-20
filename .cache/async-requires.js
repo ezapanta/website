6cadccadc5901ceb7636fcaf8e5afe72ba2098ad
Eddy Zapanta <ezapanta@digitalmint.io>
2019-10-20 16:17:11 -0500

    production build

---
+++ .cache/async-requires.js
@@ -0,0 +1,10 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-pages-404-js": () => import("/Users/ed/code/ezapanta.github.io/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-experience-js": () => import("/Users/ed/code/ezapanta.github.io/src/pages/experience.js" /* webpackChunkName: "component---src-pages-experience-js" */),
+  "component---src-pages-index-js": () => import("/Users/ed/code/ezapanta.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("/Users/ed/code/ezapanta.github.io/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+}
+
