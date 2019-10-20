76e62bc9beef263f2ed0d32ba76f3156f1059afd
Eddy Zapanta <ezapanta@digitalmint.io>
2019-10-20 15:50:32 -0500

    no deploy

---
+++ src/pages/experience.js
@@ -0,0 +1,85 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
+import Name from "../../public/icons/Name";
+import "../scss/main.scss"
+
+const Experience = () => (
+  // <Layout>
+  //   <SEO title="Home" />
+  //   <h1>Hi people</h1>
+  //   <p>Welcome to your new Gatsby site.</p>
+  //   <p>Now go build something great.</p>
+  //   <div styles={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  //     <Image />
+  //   </div>
+  //   <Link to="/page-2/">Go to page 2</Link>
+  // </Layout>
+  // <div class="vertical-center text-center">
+  <div class="vertical-center" style={{ color: '#FFFFFF' }}>
+    <div class="dead-center">
+      <h1 class="text-center fadeTitle" style={{ color: '#FFFFFF' }}>Experience</h1>
+      <div class="container margin-bottom first">
+        <div class="flex-row">
+          <strong class="flex-large no-margin-bottom">Jr. Software Engineer @ DigitalMint</strong>
+        </div>
+        <div class="flex-row">
+          <p class="flex-large">Sep. 2019 - Present</p>
+        </div>
+        <div class="flex-row">
+          <div class="flex-large">
+            Launched Bravo, a management platform for cryptocurrency exchanges. I mainly develop frontend features written with React.
+            Occassionally, I peek into the backend as an opportunity to learn Go.
+          </div>
+        </div>
+      </div>
+      <div class="container margin-bottom second">
+        <div class="flex-row">
+          <strong class="flex-large no-margin-bottom">Software Engineer @ Backbar</strong>
+        </div>
+        <div class="flex-row">
+          <p class="flex-large">Jun. 2019 - Sep. 2019</p>
+        </div>
+        <div class="flex-row">
+          <div class="flex-large">
+            A management tool beverages programs at bars and restaurants.
+            After launch, I continued to maintain the mobile application in addition to its Vue web application,
+            and the backend which consists of a Node/Express API and MySQL database. Visit the {' '}
+              <a href="https://www.getbackbar.com/">
+                <u class="link">info site</u>
+              </a>
+            {' to learn more!'}
+          </div>
+        </div>
+      </div>
+      <div class="container margin-bottom third">
+        <div class="flex-row">
+          <strong class="flex-large no-margin-bottom">Mobile Development Intern @ Backbar</strong>
+        </div>
+        <div class="flex-row">
+          <p class="flex-large">Jan. 2019 - Jun. 2019</p>
+        </div>
+        <div class="flex-row">
+          <div class="flex-large">
+            Developed React Native management tool beverages programs at bars and restaurants. Features include real-time inventory counting,
+            menu information updates, and more! Download Backbar (for free!) on the {' '}
+              <span>
+                <a href="https://apps.apple.com/us/app/backbar/id1461796151">
+                  <u class="link">App Store</u>
+                </a>
+              </span>
+              {' and '}
+              <a href="https://play.google.com/store/apps/details?id=com.backbar&hl=en_US">
+                <u class="link">Google Play</u>
+              </a>.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Experience
