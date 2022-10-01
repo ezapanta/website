02675023a85ecc4a08d3aab890760910cc702c30
Eddy Zapanta <ezapanta@digitalmint.io>
2022-10-01 15:38:47 -0500

    update

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -1,10 +1,9 @@
 // prefer default export if available
-const preferDefault = m => m && m.default || m
+const preferDefault = m => (m && m.default) || m
 
 exports.components = {
-  "component---src-pages-404-js": () => import("/Users/ed/code/ezapanta.github.io/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-experience-js": () => import("/Users/ed/code/ezapanta.github.io/src/pages/experience.js" /* webpackChunkName: "component---src-pages-experience-js" */),
-  "component---src-pages-index-js": () => import("/Users/ed/code/ezapanta.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-page-2-js": () => import("/Users/ed/code/ezapanta.github.io/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-experience-js": () => import("./../../../src/pages/experience.js" /* webpackChunkName: "component---src-pages-experience-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,60 +1,49 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
+import Layout from "../components/layout";
 // import Image from "../components/image"
 // import SEO from "../components/seo"
 import Name from "../../public/icons/Name";
-import "../scss/main.scss"
+import "../scss/main.scss";
 
 const IndexPage = () => (
-  // <Layout>
-  //   <SEO title="Home" />
-  //   <h1>Hi people</h1>
-  //   <p>Welcome to your new Gatsby site.</p>
-  //   <p>Now go build something great.</p>
-  //   <div styles={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-  //     <Image />
-  //   </div>
-  //   <Link to="/page-2/">Go to page 2</Link>
-  // </Layout>
-  // <div class="vertical-center text-center">
   <div class="vertical-center">
     <div class="container dead-center">
-        <svg
-          width="671.5"
-          height="90"
-          viewBox="0 0 671.5 90"
-          xmlns="http://www.w3.org/2000/svg"
+      <svg
+        width="671.5"
+        height="90"
+        viewBox="0 0 671.5 90"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g
+          id="svgGroup"
+          stroke-linecap="round"
+          fill-rule="evenodd"
+          font-size="9pt"
+          stroke="#FFF"
+          stroke-width="0.25mm"
+          fill="none"
+          styles="stroke:#FFF;stroke-width:0.25mm;fill:none"
         >
-          <g
-            id="svgGroup"
-            stroke-linecap="round"
-            fill-rule="evenodd"
-            font-size="9pt"
-            stroke="#FFF"
-            stroke-width="0.25mm"
-            fill="none"
-            styles="stroke:#FFF;stroke-width:0.25mm;fill:none"
-          >
-            <path
-              class="name"
-              d={Name}
-              vector-effect="non-scaling-stroke"
-            />
-          </g>
-        </svg>
+          <path class="name" d={Name} vector-effect="non-scaling-stroke" />
+        </g>
+      </svg>
       <div class="flex-row">
         <div class="flex-small link">
-          <Link to="/experience/" class="white first">Experience</Link>
+          <Link to="/experience/" class="white first">
+            Experience
+          </Link>
         </div>
         <div class="flex-small link">
-          <a href="/ezapanta.pdf" class="white second">Resume</a>
+          <a href="/ezapanta.pdf" class="white second">
+            Resume
+          </a>
         </div>
       </div>
       <h5 class="text-center current">Jr. Software Engineer @ DigitalMint</h5>
-    </div>  
+    </div>
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
